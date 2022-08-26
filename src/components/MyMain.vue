2ecfbdba44cdc81bd5e1a71848bbd4a61862f25f
ErikBorgogno <erikborgo.20@gmail.com>
2022-08-26 22:18:49 +0200

    bonus done

--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -1,6 +1,7 @@
 <template>
     <main class="container-fluid g-0 ms_bg">
-        <div class="d-block d-sm-flex align-items-center flex-wrap m-auto ms_wi">
+        <LoadProgress v-if="loadProgress"/>
+        <div v-else class="d-block d-sm-flex align-items-center flex-wrap m-auto ms_wi">
             <SingleCard v-for="disc in discList" :key="disc.id" :disc="disc"/>
         </div>
     </main>
@@ -10,16 +11,19 @@
 
 import axios from 'axios';
 import SingleCard from './SingleCard.vue';
+import LoadProgress from './LoadProgress.vue';
 
 export default {
     name: "MyMain",
-    components: { 
-        SingleCard 
-    },
+    components: {
+    SingleCard,
+    LoadProgress
+},
     data(){
         return{
             discList: [],
-            api: 'https://flynn.boolean.careers/exercises/api/array/music', 
+            api: 'https://flynn.boolean.careers/exercises/api/array/music',
+            loadProgress: true 
         }
     },
     mounted(){
@@ -29,11 +33,14 @@ export default {
         getDiscMusic(){
             axios.get(this.api)
             .then(response =>{
-                console.log(response);
+                // console.log(response);
                 this.discList = response.data.response;
+                this.loadProgress = false;
+                console.log(this.loadProgress)
             })
             .catch(err => {
                 console.log(err);
+                this.loadProgress = false;
             });
         }
     }
